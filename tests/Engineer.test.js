c7065c16fffa4d37517aaf0269b31367b0c1d335
sunita persad <sunitapersad020@gmail.com>
2021-10-31 14:18:00 -0400

    updating test files

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,11 +2,12 @@ const Engineer = require("../lib/Engineer");
 
 test("Ability to set GitHub username using constructor function", () => {
     const github = "malloryfaria";
-    const employee = new Engineer("Mallory", 1, "mallory.faria@gmail.com", github);
+    const employee = new Engineer("Sunita", 1, "sunita.persad020@gmail.com", github);
     expect(employee.github).toBe(github);
   });
 
   test("getRole() should return Engineer as a role", () => {
     const role = "Engineer";
-    const employee = new Engineer("Mallory", 1, "mallory.faria@gmail.com", "malloryfaria");
-    expect(employee.getRole())
+    const employee = new Engineer("Sunita", 1, "sunita.persad020@gmail.com", "sunitapersad");
+    expect(employee.getRole()).toBe(role);
+  });
