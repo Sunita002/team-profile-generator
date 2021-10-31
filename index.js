b0939efae8508e47ab3ac71f893d1bb78f105be4
sunita persad <sunitapersad020@gmail.com>
2021-10-31 12:37:10 -0400

    adding required packages

--- index.js
+++ index.js
@@ -5,8 +5,3 @@ const path = require("path");
 const fileDirectory = path.resolve(__dirname, "dist");
 const filePath = path.join(fileDirectory, "index.html");
 
-// Required module exports
-const Engineer = require("");
-const Intern = require("");
-const Manager = require("");
-const renderHTML = require("");
