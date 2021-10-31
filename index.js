dfdeb5ca616358e80b866afea565287e230dd77a
sunita persad <sunitapersad020@gmail.com>
2021-10-31 12:17:01 -0400

    setting up initial files and index.js required packages

--- index.js
+++
@@ -1,6 +0,0 @@
-// Required packages
-const inquirer = require("inquirer");
-const fs = require("fs");
-const path = require("path");
-const fileDirectory = path.resolve(__dirname, "dist");
-const filePath = path.join(fileDirectory, "index.html");
