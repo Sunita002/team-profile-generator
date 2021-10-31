47b4088f84e94a67690eaf43eb4b867cd05f1cdc
sunita persad <sunitapersad020@gmail.com>
2021-10-31 12:43:16 -0400

    updating module exports and adding employee array

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const path = require("path");
 const fileDirectory = path.resolve(__dirname, "dist");
 const filePath = path.join(fileDirectory, "index.html");
 
+// Required module exports
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const renderHTML = require("./lib/generateHTML");
